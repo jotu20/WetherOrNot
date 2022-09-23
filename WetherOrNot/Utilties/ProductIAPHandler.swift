8e43083232cbbb4e145d575ea287e5ce5a81fd9b
Joseph Szafarowicz <joeszafarowicz@gmail.com>
2022-09-23 12:55:39 -0400

    Add IAP products

---
+++ WetherOrNot/Utilties/ProductIAPHandler.swift
@@ -0,0 +1,127 @@
+//
+//  ProductIAPHandler.swift
+//  WetherOrNot
+//
+//  Created by Joseph Szafarowicz on 9/23/22.
+//
+
+import Foundation
+import StoreKit
+
+enum ProductIAPHandlerAlertType {
+    case setProductIds
+    case disabled
+    case restored
+    case purchased
+    
+    var message: String {
+        switch self {
+        case .setProductIds: return "Product ids not set, call setProductIds method!"
+        case .disabled: return "Purchases are disabled on your device!"
+        case .restored: return "You've successfully restored your purchase!"
+        case .purchased: return "You've successfully bought this purchase!"
+        }
+    }
+}
+
+class ProductIAPHandler: NSObject {
+    static let shared = ProductIAPHandler()
+    private override init() { }
+    
+    fileprivate var productIds = [String]()
+    fileprivate var productID = ""
+    fileprivate var productsRequest = SKProductsRequest()
+    fileprivate var fetchProductCompletion: (([SKProduct])->Void)?
+    
+    fileprivate var productToPurchase: SKProduct?
+    fileprivate var purchaseProductCompletion: ((ProductIAPHandlerAlertType, SKProduct?, SKPaymentTransaction?)->Void)?
+    
+    var isLogEnabled: Bool = true
+    
+    func setProductIds(ids: [String]) {
+        self.productIds = ids
+    }
+
+    func canMakePurchases() -> Bool { return SKPaymentQueue.canMakePayments() }
+    
+    func purchase(product: SKProduct, completion: @escaping ((ProductIAPHandlerAlertType, SKProduct?, SKPaymentTransaction?)->Void)) {
+        self.purchaseProductCompletion = completion
+        self.productToPurchase = product
+
+        if self.canMakePurchases() {
+            let payment = SKPayment(product: product)
+            SKPaymentQueue.default().add(self)
+            SKPaymentQueue.default().add(payment)
+            
+            log("PRODUCT TO PURCHASE: \(product.productIdentifier)")
+            productID = product.productIdentifier
+        } else {
+            completion(ProductIAPHandlerAlertType.disabled, nil, nil)
+        }
+    }
+    
+    func restorePurchase() {
+        SKPaymentQueue.default().add(self)
+        SKPaymentQueue.default().restoreCompletedTransactions()
+    }
+    
+    func fetchAvailableProducts(completion: @escaping (([SKProduct])->Void)){
+        self.fetchProductCompletion = completion
+        
+        if self.productIds.isEmpty {
+            log(ProductIAPHandlerAlertType.setProductIds.message)
+            fatalError(ProductIAPHandlerAlertType.setProductIds.message)
+        } else {
+            productsRequest = SKProductsRequest(productIdentifiers: Set(self.productIds))
+            productsRequest.delegate = self
+            productsRequest.start()
+        }
+    }
+    
+    fileprivate func log <T> (_ object: T) {
+        if isLogEnabled {
+            NSLog("\(object)")
+        }
+    }
+}
+
+extension ProductIAPHandler: SKProductsRequestDelegate, SKPaymentTransactionObserver {
+    func productsRequest (_ request:SKProductsRequest, didReceive response:SKProductsResponse) {
+        if (response.products.count > 0) {
+            if let completion = self.fetchProductCompletion {
+                completion(response.products)
+            }
+        }
+    }
+    
+    func paymentQueueRestoreCompletedTransactionsFinished(_ queue: SKPaymentQueue) {
+        if let completion = self.purchaseProductCompletion {
+            completion(ProductIAPHandlerAlertType.restored, nil, nil)
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction:AnyObject in transactions {
+            if let trans = transaction as? SKPaymentTransaction {
+                switch trans.transactionState {
+                case .purchased:
+                    log("Product purchase done")
+                    SKPaymentQueue.default().finishTransaction(transaction as! SKPaymentTransaction)
+                    if let completion = self.purchaseProductCompletion {
+                        completion(ProductIAPHandlerAlertType.purchased, self.productToPurchase, trans)
+                    }
+                    break
+                    
+                case .failed:
+                    log("Product purchase failed")
+                    SKPaymentQueue.default().finishTransaction(transaction as! SKPaymentTransaction)
+                    break
+                case .restored:
+                    log("Product restored")
+                    SKPaymentQueue.default().finishTransaction(transaction as! SKPaymentTransaction)
+                    break
+                    
+                default: break
+                }}}
+    }
+}
--- WetherOrNot/View/Controllers/SettingsVC.swift
+++ WetherOrNot/View/Controllers/SettingsVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import StoreKit
 
 class SettingsVC: UITableViewController {
 
@@ -25,6 +26,8 @@ class SettingsVC: UITableViewController {
     @IBOutlet weak var themeButton8: UIButton!
     @IBOutlet weak var themeButton9: UIButton!
     
+    var productsArray = [SKProduct]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.title = "Settings"
@@ -34,6 +37,13 @@ class SettingsVC: UITableViewController {
         drizzleTipButton.layer.cornerRadius = CGFloat(GlobalVariables.sharedInstance.cornerRadius)
         showerTipButton.layer.cornerRadius = CGFloat(GlobalVariables.sharedInstance.cornerRadius)
         downpourTipButton.layer.cornerRadius = CGFloat(GlobalVariables.sharedInstance.cornerRadius)
+        
+        ProductIAPHandler.shared.setProductIds(ids: ["com.dropletTip", "com.drizzleTip", "com.showerTip", "com.downpourTip"])
+        ProductIAPHandler.shared.fetchAvailableProducts { [weak self] (products) in
+//            guard let sSelf = self else {return}
+//            sSelf.productsArray = products
+            self?.productsArray = products
+         }
     }
     
     @IBAction func themeButton0Tapped(_ sender: UIButton) {
@@ -77,14 +87,74 @@ class SettingsVC: UITableViewController {
     }
     
     @IBAction func dropletTipButtonTapped(_ sender: UIButton) {
+        ProductIAPHandler.shared.purchase(product: self.productsArray[2]) { (alert, product, transaction) in
+           if let tran = transaction, let prod = product {
+               print(tran, prod)
+               
+               if tran.transactionState == .purchased {
+                   let alert = UIAlertController(title: "Thank you!", message: "Your purchase is much appreciated and helps fund the services for this app.", preferredStyle: .alert)
+                   alert.addAction(UIAlertAction(title: "Done", style: .default))
+                   self.present(alert, animated: true)
+               } else {
+                   let alert = UIAlertController(title: "", message: "Looks like someting went wrong!", preferredStyle: .alert)
+                   alert.addAction(UIAlertAction(title: "OK", style: .default))
+                   self.present(alert, animated: true)
+               }
+           }
+        }
     }
     
     @IBAction func drizzleTipButtonTapped(_ sender: UIButton) {
+        ProductIAPHandler.shared.purchase(product: self.productsArray[1]) { (alert, product, transaction) in
+           if let tran = transaction, let prod = product {
+               print(tran, prod)
+               
+               if tran.transactionState == .purchased {
+                   let alert = UIAlertController(title: "Thank you!", message: "Your purchase is much appreciated and helps fund the services for this app.", preferredStyle: .alert)
+                   alert.addAction(UIAlertAction(title: "Done", style: .default))
+                   self.present(alert, animated: true)
+               } else {
+                   let alert = UIAlertController(title: "", message: "Looks like someting went wrong!", preferredStyle: .alert)
+                   alert.addAction(UIAlertAction(title: "OK", style: .default))
+                   self.present(alert, animated: true)
+               }
+           }
+        }
     }
     
     @IBAction func showerTipButtonTapped(_ sender: UIButton) {
+        ProductIAPHandler.shared.purchase(product: self.productsArray[3]) { (alert, product, transaction) in
+           if let tran = transaction, let prod = product {
+               print(tran, prod)
+               
+               if tran.transactionState == .purchased {
+                   let alert = UIAlertController(title: "Thank you!", message: "Your purchase is much appreciated and helps fund the services for this app.", preferredStyle: .alert)
+                   alert.addAction(UIAlertAction(title: "Done", style: .default))
+                   self.present(alert, animated: true)
+               } else {
+                   let alert = UIAlertController(title: "", message: "Looks like someting went wrong!", preferredStyle: .alert)
+                   alert.addAction(UIAlertAction(title: "OK", style: .default))
+                   self.present(alert, animated: true)
+               }
+           }
+        }
     }
     
     @IBAction func downpourTipButtonTapped(_ sender: UIButton) {
+        ProductIAPHandler.shared.purchase(product: self.productsArray[0]) { (alert, product, transaction) in
+           if let tran = transaction, let prod = product {
+               print(tran, prod)
+               
+               if tran.transactionState == .purchased {
+                   let alert = UIAlertController(title: "Thank you!", message: "Your purchase is much appreciated and helps fund the services for this app.", preferredStyle: .alert)
+                   alert.addAction(UIAlertAction(title: "Done", style: .default))
+                   self.present(alert, animated: true)
+               } else {
+                   let alert = UIAlertController(title: "", message: "Looks like someting went wrong!", preferredStyle: .alert)
+                   alert.addAction(UIAlertAction(title: "OK", style: .default))
+                   self.present(alert, animated: true)
+               }
+           }
+        }
     }
 }
